9a244d31ffdd14ccff9186875a578f541c4447db
Evellyn Jade <97754536+evellynreis@users.noreply.github.com>
2023-11-28 08:36:21 -0300

    update api for the movie

--- php/api.php
+++ php/api.php
@@ -3,11 +3,11 @@ require_once('vendor/autoload.php');
 
 $client = new \GuzzleHttp\Client();
 
-$response = $client->request('GET', 'https://api.themoviedb.org/3/account/16372688/favorite/movies?language=en-US&page=1&sort_by=created_at.asc', [
-    'headers' => [
-        'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MGU0MGQyOGZjMmU5NDMzNGUyZjhlZjMxNWY0MzMyZCIsInN1YiI6IjYzOTNkZTE1OWY1ZGZiMDA5MTZjODNkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pPu9uJsyC7UWNL8knICyo3kX-tj6Fk2aGNkogkIgznw',
-        'accept' => 'application/json',
-    ],
+$response = $client->request('GET', 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', [
+  'headers' => [
+    'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MGU0MGQyOGZjMmU5NDMzNGUyZjhlZjMxNWY0MzMyZCIsInN1YiI6IjYzOTNkZTE1OWY1ZGZiMDA5MTZjODNkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pPu9uJsyC7UWNL8knICyo3kX-tj6Fk2aGNkogkIgznw',
+    'accept' => 'application/json',
+  ],
 ]);
 
 echo $response->getBody();
